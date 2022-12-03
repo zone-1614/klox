ccb4bf33bed635784eaad44654ddb01c663632e8
Zone <2493920954@qq.com>
2022-12-03 14:57:52 +0800

    lexer

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,54 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.nio.charset.Charset
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.system.exitProcess
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    if (args.size > 1) {
+        println("Usage: klox [script]")
+        exitProcess(64)
+    } else if (args.size == 1) {
+        runFile(args[0])
+    } else {
+        runPrompt()
+    }
+}
+
+var hadError = false
+
+private fun runFile(path: String) {
+    val bytes = Files.readAllBytes(Paths.get(path))
+//    run(String(bytes), Charset.defaultCharset())
+    if (hadError)
+        exitProcess(65)
+}
+
+private fun runPrompt() {
+    val input = InputStreamReader(System.`in`)
+    val reader = BufferedReader(input)
+    while (true) {
+        print("> ")
+        val line = reader.readLine()
+        if (line == null)
+            break;
+        run(line)
+        hadError = false
+    }
+}
+
+private fun run(source: String) {
+    val scanner = Scanner(source)
+    val tokens = scanner.scanTokens()
+
+}
+
+fun error(line: Int, message: String) {
+    report(line, "", message)
+}
+
+private fun report(line: Int, where: String, message: String) {
+    println("[line $line] Error $where: $message")
+    hadError = true
 }
---
+++ src/main/kotlin/Scanner.kt
@@ -0,0 +1,144 @@
+class Scanner(private val source: String) {
+    private val tokens: MutableList<Token> = arrayListOf()
+    private var start: Int = 0
+    private var current: Int = 0
+    private var line: Int = 1
+
+    companion object {
+        private val keywords: Map<String, TokenType> = mapOf(
+            "and" to TokenType.AND,
+            "class" to TokenType.CLASS,
+            "else" to TokenType.ELSE,
+            "false" to TokenType.FALSE,
+            "for" to TokenType.FOR,
+            "fun" to TokenType.FUN,
+            "if" to TokenType.IF,
+            "nil" to TokenType.NIL,
+            "or" to TokenType.OR,
+            "print" to TokenType.PRINT,
+            "return" to TokenType.RETURN,
+            "super" to TokenType.SUPER,
+            "this" to TokenType.THIS,
+            "true" to TokenType.TRUE,
+            "var" to TokenType.VAR,
+            "while" to TokenType.WHILE
+        )
+    }
+
+    fun scanTokens(): List<Token> {
+        while (!isAtEnd()) {
+            start = current
+            scanToken()
+        }
+        tokens.add(Token(TokenType.EOF, "", null, line))
+        return tokens
+    }
+
+    private fun isAtEnd(): Boolean = current >= source.length
+
+    private fun scanToken() {
+        val c: Char = advance()
+        when (c) {
+            '(' -> addToken(TokenType.LEFT_PAREN)
+            ')' -> addToken(TokenType.RIGHT_PAREN)
+            '{' -> addToken(TokenType.LEFT_BRACE)
+            '}' -> addToken(TokenType.RIGHT_BRACE)
+            ',' -> addToken(TokenType.COMMA)
+            '.' -> addToken(TokenType.DOT)
+            '-' -> addToken(TokenType.MINUS)
+            '+' -> addToken(TokenType.PLUS)
+            ';' -> addToken(TokenType.SEMICOLON)
+            '*' -> addToken(TokenType.STAR)
+
+            '!' -> addToken(if (match('=')) { TokenType.BANG_EQUAL } else { TokenType.BANG })
+            '=' -> addToken(if (match('=')) { TokenType.EQUAL_EQUAL } else { TokenType.EQUAL })
+            '<' -> addToken(if (match('=')) { TokenType.LESS_EQUAL } else { TokenType.LESS })
+            '>' -> addToken(if (match('=')) { TokenType.GREATER_EQUAL } else { TokenType.GREATER })
+
+            '/' -> {
+                if (match('/')) {
+                    while (peek() != '\n' && !isAtEnd()) advance() // comment
+                } else {
+                    addToken(TokenType.SLASH)
+                }
+            }
+            // ignore whitespace
+            ' ' -> { }
+            '\r' -> { }
+            '\t' -> { }
+
+            '\n' -> line++
+
+            '"' -> string()
+            else -> {
+                if (isDigit(c)) {
+                    number()
+                } else if (isAlpha(c)) {
+                    identifier()
+                } else {
+                    error(line, "Unexpected character. ")
+                }
+            }
+        }
+    }
+
+    private fun match(expected: Char): Boolean {
+        if (isAtEnd()) return false
+        if (source[current] != expected) return false
+        current++
+        return true
+    }
+
+    private fun advance(): Char = source[current++]
+
+    private fun addToken(type: TokenType) = addToken(type, null)
+
+    private fun addToken(type: TokenType, literal: Any?) {
+        val text = source.substring(start, current)
+        tokens.add(Token(type, text, literal, line))
+    }
+
+    private fun peek(): Char = if (isAtEnd()) { '\n' } else { source[current] }
+    private fun peekNext(): Char = if (current + 1 >= source.length) { '\n' } else { source[current + 1] }
+
+    private fun string() {
+        while (peek() != '"' && !isAtEnd()) {
+            if (peek() == '\n')
+                line++
+            advance()
+        }
+        if (isAtEnd()) {
+            error(line, "Unterminated string. ")
+            return
+        }
+        advance()
+        val value = source.substring(start + 1, current - 1)
+        addToken(TokenType.STRING, value)
+    }
+
+    private fun isDigit(c: Char): Boolean = c in '0'..'9'
+    private fun isAlpha(c: Char): Boolean = (c in 'a'..'z').or(c in 'A'..'Z').or(c == '_')
+    private fun isAlphaNumeric(c: Char): Boolean = isDigit(c).or(isAlpha(c))
+
+    private fun number() {
+        while (isDigit(peek()))
+            advance()
+        if (peek() == '.' && isDigit(peekNext())) {
+            advance() // consume '.'
+            while (isDigit(peek()))
+                advance()
+        }
+        addToken(TokenType.NUMBER, source.substring(start, current).toDouble())
+    }
+
+    private fun identifier() {
+        while (isAlphaNumeric(peek()))
+            advance()
+        val text = source.substring(start, current)
+        var type = keywords[text]
+        type?.let {
+            type = TokenType.IDENTIFIER
+        }
+        addToken(type!!)
+    }
+}
