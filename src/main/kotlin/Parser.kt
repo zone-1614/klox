109a8ee22d90c8ecfcf4750cc0c62aae5b95e36c
Zone <2493920954@qq.com>
2022-12-06 19:12:34 +0800

    interpret

--- src/main/kotlin/Parser.kt
+++ src/main/kotlin/Parser.kt
@@ -11,8 +11,14 @@ class Parser(private val tokens: List<Token>) {
         }
     }
 
+    /**
+     * expression -> equality
+     */
     private fun expression(): Expr = equality()
 
+    /**
+     * equality -> comparison ( ( "!=" | "==" ) comparison )*
+     */
     private fun equality(): Expr {
         var expr = comparison()
         while (match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL)) {
@@ -23,6 +29,9 @@ class Parser(private val tokens: List<Token>) {
         return expr
     }
 
+    /**
+     * comparison -> term ( ( ">" | ">=" | "<" | "<=" ) term )*
+     */
     private fun comparison(): Expr {
         var expr = term()
         while (match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL)) {
@@ -33,6 +42,9 @@ class Parser(private val tokens: List<Token>) {
         return expr
     }
 
+    /**
+     * term -> factor ( ( "-" | "+" ) factor )*
+     */
     private fun term(): Expr {
         var expr = factor()
         while (match(TokenType.PLUS, TokenType.MINUS)) {
@@ -43,6 +55,9 @@ class Parser(private val tokens: List<Token>) {
         return expr
     }
 
+    /**
+     * factor -> unary ( ( "*" | "/" ) unary )*
+     */
     private fun factor(): Expr {
         var expr = unary()
         while (match(TokenType.SLASH, TokenType.STAR)) {
@@ -53,6 +68,10 @@ class Parser(private val tokens: List<Token>) {
         return expr
     }
 
+    /**
+     * unary -> ( "!" | "-" ) unary
+     *        | primary
+     */
     private fun unary(): Expr {
         while (match(TokenType.BANG, TokenType.MINUS)) {
             val operator = previous()
@@ -62,11 +81,18 @@ class Parser(private val tokens: List<Token>) {
         return primary()
     }
 
+    /**
+     * primary -> NUMBER | STRING | "true" | "false" | "nil" | "(" expression ")"
+     */
     private fun primary(): Expr {
-        if (match(TokenType.NUMBER, TokenType.STRING)) return Expr.Companion.Literal(previous().literal)
-        if (match(TokenType.TRUE)) return Expr.Companion.Literal(true)
-        if (match(TokenType.FALSE)) return Expr.Companion.Literal(false)
-        if (match(TokenType.NIL)) return Expr.Companion.Literal(null)
+        if (match(TokenType.FALSE))
+            return Expr.Companion.Literal(false)
+        if (match(TokenType.TRUE))
+            return Expr.Companion.Literal(true)
+        if (match(TokenType.NIL))
+            return Expr.Companion.Literal(null)
+        if (match(TokenType.NUMBER, TokenType.STRING))
+            return Expr.Companion.Literal(previous().literal)
         if (match(TokenType.LEFT_PAREN)) {
             val expr = expression()
             consume(TokenType.RIGHT_PAREN, "Except ')' after expression. ")
@@ -91,9 +117,9 @@ class Parser(private val tokens: List<Token>) {
     }
 
     private fun check(type: TokenType): Boolean {
-        if (!isAtEnd())
+        if (isAtEnd())
             return false
-        return peek().type == type
+        return type == peek().type
     }
 
     private fun advance(): Token {
